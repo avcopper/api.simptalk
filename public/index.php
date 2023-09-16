b140fa8ba6a431e7c049c015bc062905dbecea32
andrew <avcopper@mail.ru>
2023-09-16 14:39:50 +0600

    index

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-require __DIR__ . '/../app/System/ErrorSupervisor.php';
 require __DIR__ . '/../config/autoload.php';
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config/constants.php';
+require __DIR__ . '/../app/System/ErrorSupervisor.php';
 
 use System\Route;
 use System\Response;
@@ -15,9 +15,9 @@ Security::array_xss_clean($_PUT);
 Security::array_xss_clean($_DELETE);
 Security::array_xss_clean($_PATCH);
 
-Route::parseUrl($_SERVER['REQUEST_URI']);
 
 try {
+    Route::parseUrl($_SERVER['REQUEST_URI']);
     Route::startApi();
 } catch (Exception $e) {
     (new Response($e->getCode(), false, $e->getMessage()))->send();
